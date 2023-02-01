4630bd46b2e4a5331249f1f33966ed286178f7cc
therealhesham <heshammoha231992@gmail.com>
2023-02-01 13:27:47 +0200

    s

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ const allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     
-    // res.header("Access-Control-Allow-Credentials","true");
+    res.header("Access-Control-Allow-Credentials","true");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }

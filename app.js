0bcf421a9ad87512be8408d1d87f9733186ea25f
therealhesham <heshammoha231992@gmail.com>
2023-02-01 23:52:18 +0200

    s

--- app.js
+++ app.js
@@ -31,17 +31,17 @@ app.use(express.json())
 
 const { createProxyMiddleware } = require('http-proxy-middleware')
   
-//   app.use('/*', createProxyMiddleware({ 
-//     target: 'https://localhost:3001', //original url
-//     changeOrigin: true, 
-//     //secure: false,
-//     onProxyRes: function (proxyRes, req, res) {
-//        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-//        proxyRes.headers['Access-Control-Allow-Methods'] = ['GET','POST','HEAD','PUT','PATCH','DELETE'];
-//     }
-// }));
+  app.use('/*', createProxyMiddleware({ 
+    target: 'https://localhost:3001', //original url
+    changeOrigin: true, 
+    //secure: false,
+    onProxyRes: function (proxyRes, req, res) {
+       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+       proxyRes.headers['Access-Control-Allow-Methods'] = ['GET','POST','HEAD','PUT','PATCH','DELETE'];
+    }
+}));
   
-app.use(cors({credentials:false}));
+app.use(cors({credentials:true}));
 
 function MiddleWareFunctionForLogin(req,res,next){
 console.log(req.hostname)

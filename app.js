d4803a3336d25499583f78db7e1d613a75bd3ea8
therealhesham <heshammoha231992@gmail.com>
2023-02-01 10:57:09 +0200

    add to cors

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ app.use(appSecondTransaction)
 app.use(appFourthTransction)
 app.use(helmet())
 app.use(appThirdTransaction)
-app.use(cors({credentials:true,maxAge:5555555555}))
+app.use(cors())
 app.use(appLogin)
 app.use(appRegisterNew)
 

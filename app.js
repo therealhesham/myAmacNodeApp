8dbbd6a52112b71add33573af467c3e3630a627b
therealhesham <heshammoha231992@gmail.com>
2023-02-02 21:40:36 +0200

    hello finally

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(express.json())
 //     }
 // }));
 app.use(cookieParser())  
-// app.use(cors());
+app.use(cors());
 
 function MiddleWareFunctionForLogin(req,res,next){
 console.log(req.hostname)

2f3eb519614fb6411f9f5e80f912e5ec061730ac
therealhesham <heshammoha231992@gmail.com>
2023-02-02 19:43:40 +0200

    hello finally

--- app.js
+++ app.js
@@ -31,17 +31,17 @@ app.use(express.json())
 
 const { createProxyMiddleware } = require('http-proxy-middleware')
   
-  app.use('https://my-amac-react-app.vercel.app/', createProxyMiddleware({ 
-    target: '*', //original url
-    changeOrigin: true, 
-    //secure: false,
-    onProxyRes: function (proxyRes, req, res) {
-       proxyRes.headers['Access-Control-Allow-Origin'] = 'https://my-amac-react-app.vercel.app/';
-       proxyRes.headers['Access-Control-Allow-Methods'] = ['GET','POST','HEAD','PUT','PATCH','DELETE'];
-    }
-}));
+//   app.use('https://my-amac-react-app.vercel.app/', createProxyMiddleware({ 
+//     target: '*', //original url
+//     changeOrigin: true, 
+//     //secure: false,
+//     onProxyRes: function (proxyRes, req, res) {
+//        proxyRes.headers['Access-Control-Allow-Origin'] = 'https://my-amac-react-app.vercel.app/';
+//        proxyRes.headers['Access-Control-Allow-Methods'] = ['GET','POST','HEAD','PUT','PATCH','DELETE'];
+//     }
+// }));
 app.use(cookieParser())  
-app.use(cors({credentials:false}));
+// app.use(cors({credentials:false}));
 
 function MiddleWareFunctionForLogin(req,res,next){
 console.log(req.hostname)

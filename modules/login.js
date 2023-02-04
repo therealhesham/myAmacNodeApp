a235377f82686ef6b849b79f065d1afa19fc91d4
therealhesham <heshammoha231992@gmail.com>
2023-02-04 22:56:11 +0200

    login

--- modules/login.js
+++ modules/login.js
@@ -4,19 +4,22 @@ const { loginHandleMongo } = require("./registeruser")
 const Cookies = require("universal-cookie")
 require('dotenv').config({ debug: true })
 appLogin=express()
+function MidLogin(req,res,next){
 
-
-
-appLogin.post("/login",async (req,res)=>{
     res.header({"Access-Control-Allow-Origin": "https://my-amac-react-app.vercel.app"});
-    // res.set({"Access-Control-Allow-Origin": "https://my-amac-react-app.vercel.app"});
-    // // res.setHeader({"Access-Control-Allow-Origin": "https://my-amac-react-app.vercel.app"});
+    res.set({"Access-Control-Allow-Origin": "https://my-amac-react-app.vercel.app"});
+    res.setHeader({"Access-Control-Allow-Origin": "https://my-amac-react-app.vercel.app"});
     res.header({"Access-Control-Allow-Methods": "POST"});
     res.header({"Access-Control-Allow-Credentials": "true"});
     
-    // res.set({"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"})
-    // res.header({"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"})
-    
+    res.set({"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"})
+    res.header({"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"})
+    next()
+}
+
+
+appLogin.post("/login",MidLogin,async (req,res)=>{
+  
     // Access-Control-Allow-Headers
 const email = req.body.email
 const password = req.body.password

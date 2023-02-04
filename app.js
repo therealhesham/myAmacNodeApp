a4cd8d0b616b524c25fb81fe58436453be762bd6
therealhesham <heshammoha231992@gmail.com>
2023-02-04 20:30:25 +0200

    d

--- app.js
+++ app.js
@@ -26,18 +26,18 @@ const { appFourthTransction } = require('./modules/fourthtransaction');
 const { userList, sockets } = require('./modules/users');
 const { appRegisterNew } = require('./modules/registeruser');
 const app = express();
-app.use(function(req, res, next) {
-      // res.header("Access-Control-Allow-Origin", "*");
-      const allowedOrigins = ['http://localhost:3000', 'http://my-amac-react-app.vercel.app', 'https://my-amac-react-app.vercel.app'];
-      const origin = req.headers.origin;
-      if (allowedOrigins.includes(origin)) {
-           res.setHeader({'Access-Control-Allow-Origin': origin});
-      }
-      res.header({"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"});
-      res.header({"Access-Control-Allow-credentials": true});
-      res.header({"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, UPDATE"});
-      next();
-    });
+// app.use(function(req, res, next) {
+//       // res.header("Access-Control-Allow-Origin", "*");
+//       const allowedOrigins = ['http://localhost:3000', 'http://my-amac-react-app.vercel.app', 'https://my-amac-react-app.vercel.app'];
+//       const origin = req.headers.origin;
+//       if (allowedOrigins.includes(origin)) {
+//            res.setHeader({'Access-Control-Allow-Origin': origin});
+//       }
+//       res.header({"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"});
+//       res.header({"Access-Control-Allow-credentials": true});
+//       res.header({"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, UPDATE"});
+//       next();
+//     });
 app.use(express.json())
 
 

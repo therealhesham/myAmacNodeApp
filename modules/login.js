e11510b449df215deaed025cac10d99a05c67770
therealhesham <heshammoha231992@gmail.com>
2023-02-05 09:42:35 +0200

    login

--- modules/login.js
+++ modules/login.js
@@ -29,7 +29,7 @@ const jwter = jwt.sign({username:findUser.username,
 // res.header("token",jwter)
 // res.header({"token":jwter})
 // res.set("token",jwter)
-res.cookie("token","jwter",{
+res.cookie({"token":"jwter"},{
       maxAge:  24*60*60 * 1000
 });
 

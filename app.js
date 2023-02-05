a56aad96b3f267b4ed42df513482ab6996902ad1
therealhesham <heshammoha231992@gmail.com>
2023-02-05 23:05:03 +0200

    hesham

--- app.js
+++ app.js
@@ -55,6 +55,54 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 
 
+app.post("/login",async (req,res)=>{
+  // req.session.name ="hesham"
+  const email = req.body.email
+  const password = req.body.password
+  
+  if(!email || !password) return res.send({data:"dataNotFound"});
+  
+  const findUser = await loginHandleMongo.findOne({email:email,password:password})
+  
+  
+  if (!findUser) return res.send({data:"dataNotFound"});
+  
+  
+  
+  const jwter = jwt.sign({username:findUser.username,
+      firstName:findUser.firstName},process.env.MYSECRET)
+              
+  
+  
+  res.header("token",jwter)
+  res.set("token",jwter)
+  
+  res.header({"Set-Cookie":"jwter=lllllll"})
+  res.header("Set-Cookie","jwter=lllllll")
+  res.header({"etag":"jwterlllllll"})
+  res.set("etag","jwterlllllll")
+  res.set({"Cookie":"jwter=lllllll"})
+  // res.setHeader({"eTag":"jwterlllllll"})
+  // res.clearCookie('token')
+  // res.set("token",jwter)
+  res.cookie("token","jwter"
+  // ,{
+  //       maxAge:  24*60*60 * 1000 ,samSite:"None" ,secure : false , httpOnly:false 
+  // }
+  );
+  
+  
+  res.send({data:req.session})
+  
+  
+  
+  
+  
+  
+  
+  
+  })
+
 function MiddleWareFunctionForLogin(req,res,next){
 if(req.method =="GET"){
 

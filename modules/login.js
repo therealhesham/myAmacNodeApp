816afd55bb8dc0a1e4ff8fff719cdccbcb521bf5
therealhesham <heshammoha231992@gmail.com>
2023-02-05 20:39:18 +0200

    hesham

--- modules/login.js
+++ modules/login.js
@@ -27,7 +27,7 @@ const jwter = jwt.sign({username:findUser.username,
 
 
 res.header("token",jwter)
-res.header({"Set-Cookie":"jwter"})
+res.header("Set-Cookie","jwterlllllll")
 // res.clearCookie('token')
 // res.set("token",jwter)
 res.cookie("token","jwter",{

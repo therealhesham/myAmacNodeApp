a26d1f5b732bd648aa520037fe344b6804eaed5a
therealhesham <heshammoha231992@gmail.com>
2023-02-05 23:09:41 +0200

    hesham

--- modules/login.js
+++ modules/login.js
@@ -10,44 +10,44 @@ appLogin.use(express.json())
 // appLogin.use(session({resave:false,secret:'session',cookie:{maxAge:1000*60*60,sameSite:"none",secure:true}}))
 
 
-// appLogin.post("/login",async (req,res)=>{
-// // req.session.name ="hesham"
-// const email = req.body.email
-// const password = req.body.password
+appLogin.post("/login",async (req,res)=>{
+// req.session.name ="hesham"
+const email = req.body.email
+const password = req.body.password
 
-// if(!email || !password) return res.send({data:"dataNotFound"});
+if(!email || !password) return res.send({data:"dataNotFound"});
 
-// const findUser = await loginHandleMongo.findOne({email:email,password:password})
-
-
-// if (!findUser) return res.send({data:"dataNotFound"});
+const findUser = await loginHandleMongo.findOne({email:email,password:password})
 
 
+if (!findUser) return res.send({data:"dataNotFound"});
 
-// const jwter = jwt.sign({username:findUser.username,
-//     firstName:findUser.firstName},process.env.MYSECRET)
+
+
+const jwter = jwt.sign({username:findUser.username,
+    firstName:findUser.firstName},process.env.MYSECRET)
             
 
 
-// res.header("token",jwter)
+res.header("token",jwter)
+res.set("token",jwter)
+
+res.header({"Set-Cookie":"jwter=lllllll"})
+res.header("Set-Cookie","jwter=lllllll")
+res.header({"etag":"jwterlllllll"})
+res.set("etag","jwterlllllll")
+res.set({"Cookie":"jwter=lllllll"})
+// res.setHeader({"eTag":"jwterlllllll"})
+// res.clearCookie('token')
 // res.set("token",jwter)
-
-// res.header({"Set-Cookie":"jwter=lllllll"})
-// res.header("Set-Cookie","jwter=lllllll")
-// res.header({"etag":"jwterlllllll"})
-// res.set("etag","jwterlllllll")
-// res.set({"Cookie":"jwter=lllllll"})
-// // res.setHeader({"eTag":"jwterlllllll"})
-// // res.clearCookie('token')
-// // res.set("token",jwter)
-// res.cookie("token","jwter"
-// // ,{
-// //       maxAge:  24*60*60 * 1000 ,samSite:"None" ,secure : false , httpOnly:false 
-// // }
-// );
+res.cookie("token","jwter"
+// ,{
+//       maxAge:  24*60*60 * 1000 ,samSite:"None" ,secure : false , httpOnly:false 
+// }
+);
 
 
-// res.send({data:req.session})
+res.send({data:req.headers})
 
 
 
@@ -56,5 +56,5 @@ appLogin.use(express.json())
 
 
 
-// })
+})
 module.exports.appLogin=appLogin

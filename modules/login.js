5493c01f416890bf255a18014c62b3f22d869c5c
therealhesham <heshammoha231992@gmail.com>
2023-02-07 20:49:34 +0200

    hesham

--- modules/login.js
+++ modules/login.js
@@ -16,12 +16,12 @@ appLogin.post("/login",(req,res,next)=>{
     res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
     res.header({"Access-Control-Allow-Credentials": true});
     res.header("Access-Control-Max-Age", 24*60*60*1000);
-    res.header("Set-Cookie", "sid=14A52; max-age=36000;samsite=None;samSite=none ;SamSite=None ;Secure ")
+    res.header("Set-Cookie", "sid=14A52; max-age=36000;samesite=None;sameSite=none ;SameSite=None ;Secure ")
     res.cookie("token","jwter"
 ,{
-      maxAge: 1000000000 , samSite : "None",SamSite:"None" ,
-      samsite : "None",SamSite:"none" ,
-      samSite : "None",SamSite:"None" ,httpOnly:true
+      maxAge: 1000000000 , sameSite : "None",SameSite:"None" ,
+      samesite : "None",SameSite:"none" ,
+      sameSite : "None",SameSite:"None" ,httpOnly:true
 }
 );
 

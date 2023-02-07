1fdf19fa408f3580059299ebff8026e027e91e56
therealhesham <heshammoha231992@gmail.com>
2023-02-07 11:29:49 +0200

    hesham

--- modules/login.js
+++ modules/login.js
@@ -17,13 +17,6 @@ appLogin.post("/login",(req,res,next)=>{
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Max-Age", 600000);
     res.header("Set-Cookie", "sid=14A52; max-age=60*24*24*1000;samsite=None;samSite=none ;SamSite=None ;secure ")
-    res.cookie("token","jwter"
-,{
-      maxAge: 1000000000 , samSite : "None",SamSite:"None" ,
-      samsite : "None",SamSite:"none" ,
-      samSite : "None",SamSite:"None" ,
-}
-);
 
 
 next()    
@@ -53,11 +46,9 @@ const jwter = jwt.sign({username:findUser.username,
             
 
 
-res.cookie("token",jwter,{
-    maxAge: 1000000000 , samSite : "None",SamSite:"None" ,
-    samsite : "None",SamSite:"none" ,
-    samSite : "None",SamSite:"None" ,
-})
+    res.header("Set-Cookie", "sid=14A52; max-age=60*24*24*1000;samsite=None;samSite=none ;SamSite=None ;secure ")
+
+
 // // res.set("token",jwter)
 // res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
 // res.header("Access-Control-Allow-Credentials", true);

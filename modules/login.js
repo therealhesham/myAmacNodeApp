28d551e8f07421db439395b9cd69f33206334e83
therealhesham <heshammoha231992@gmail.com>
2023-02-07 14:03:44 +0200

    hesham

--- modules/login.js
+++ modules/login.js
@@ -15,7 +15,7 @@ appLogin.use(express.json())
 appLogin.post("/login",(req,res,next)=>{
     res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
     res.header({"Access-Control-Allow-Credentials": true});
-    res.header("Access-Control-Max-Age", 600);
+    res.header("Access-Control-Max-Age", 24*60*60*1000);
     res.header("Set-Cookie", "sid=14A52; max-age=3600;samsite=None;samSite=none ;SamSite=None ;Secure ")
     res.cookie("token","jwter"
 ,{
@@ -53,7 +53,7 @@ const jwter = jwt.sign({username:findUser.username,
             
 
 
-res.cookie({"token":jwter})
+res.cookie("token",jwter,{maxAge: "24*60*60*1000"})
 // // res.set("token",jwter)
 // res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
 // res.header("Access-Control-Allow-Credentials", true);

590e120616594acfe4530d42a9e616a7f7028566
therealhesham <heshammoha231992@gmail.com>
2023-02-08 11:44:35 +0200

    hesham

--- modules/login.js
+++ modules/login.js
@@ -52,7 +52,8 @@ const jwter = jwt.sign({username:findUser.username,
     firstName:findUser.firstName},process.env.MYSECRET)
             
     res.header("Set-Cookie", "token="+jwter+"; max-age=3600;samesite=None;sameSite=none ;SameSite=None ;Secure ")
-    res.cookie("hesham","hosom",{sameSite:"none"})
+    res.cookie("hesham","hosom",{sameSite:"none" ,secure:true})
+    res.cookie("hessham","hosom",{sameSite:"none" ,secure:false})
 
 res.send(jwter)
 

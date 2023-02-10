0ca10bd9bf967824b8604236cfb83f67168582b2
therealhesham <heshammoha231992@gmail.com>
2023-02-10 18:33:37 +0200

    hesham

--- modules/joiregistervalidator.js
+++ modules/joiregistervalidator.js
@@ -11,7 +11,7 @@ const schema = Joi.object({
         .min(3)
         .max(30)
       ,
-        url: Joi.string().optional
+        url: Joi.string().default("https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/292514166_454783203318096_7743405474799047480_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=gDEMX3wbRI4AX_SJXnL&_nc_ht=scontent.fcai19-4.fna&oh=00_AfBvfL8_1w8JzU5q1_VR3WCGIxE3j2bRc12NtVo9FnJf1A&oe=63EC2E52")
         , firstName: Joi.string()
         
         .min(3)

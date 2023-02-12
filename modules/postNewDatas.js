c8c959b53d5571b50bbe2b727db6e7fc7d421d0f
therealhesham <heshammoha231992@gmail.com>
2023-02-12 12:39:20 +0200

    hesham

--- modules/postNewDatas.js
+++ modules/postNewDatas.js
@@ -8,7 +8,22 @@ const appPostNewDataTostore = expressApp()
 
 
 
-appPostNewDataTostore.post("/postnewdatatostore",async(req,res)=>
+appPostNewDataTostore.post("/postnewdatatostore",(req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
+    res.header({"Access-Control-Allow-Credentials": true});
+    res.header("Access-Control-Max-Age", 24*60*60*1000);
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+
+    const sender = req.cookies.token
+  // console.log(sender)
+  if(!sender) return res.send("not authenticated");
+  const decoder =  jwt.verify(sender,process.env.MYSECRET)
+  
+if(!decoder) return res.send("not authenticated");
+next()}
+
+,async(req,res)=>
 
 {
 

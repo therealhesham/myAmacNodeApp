549e8d6f735700779c09ede7be99585c26370d6d
therealhesham <heshammoha231992@gmail.com>
2023-02-12 15:35:26 +0200

    hesham

--- modules/registeruser.js
+++ modules/registeruser.js
@@ -169,7 +169,8 @@ next()
       const result = await cloudinary.uploader.upload(files.image.filepath);
              res.send(result.url)
          
-    } catch (error) {
+    } 
+    catch (error) {
   res.send("https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/292514166_454783203318096_7743405474799047480_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=gDEMX3wbRI4AX_SJXnL&_nc_ht=scontent.fcai19-4.fna&oh=00_AfBvfL8_1w8JzU5q1_VR3WCGIxE3j2bRc12NtVo9FnJf1A&oe=63EC2E52")    
     }
         
@@ -188,7 +189,7 @@ next()
     
 
 catch (error) {
-  console.log(error);
+res.send("https://scontent.fcai19-4.fna.fbcdn.net/v/t39.30808-6/292514166_454783203318096_7743405474799047480_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=gDEMX3wbRI4AX_SJXnL&_nc_ht=scontent.fcai19-4.fna&oh=00_AfBvfL8_1w8JzU5q1_VR3WCGIxE3j2bRc12NtVo9FnJf1A&oe=63EC2E52")    
 }
 
 })

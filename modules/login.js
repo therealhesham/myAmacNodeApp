1ed7105f9b5e0f8ab5b6951274f21a050a0c5422
therealhesham <heshammoha231992@gmail.com>
2023-02-13 19:48:03 +0200

    hesham

--- modules/login.js
+++ modules/login.js
@@ -87,7 +87,18 @@ res.send("token deleted")
 })
 
 
-appLogin.get("/checker",async(req,res)=>{
+appLogin.get("/checker",(req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
+    res.header({"Access-Control-Allow-Credentials": true});
+    
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  
+
+next()    
+
+
+},async(req,res)=>{
 
     const sender = req.cookies.token
   // console.log(sender)

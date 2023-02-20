a9ff695e9bfbe422c95c45531e758bd9dda4b99f
therealhesham <heshammoha231992@gmail.com>
2023-02-20 12:06:14 +0200

    remove headers from get requests

--- modules/users.js
+++ modules/users.js
@@ -61,26 +61,27 @@ appGettUser.post("/send",async (req,res)=>{
 
                 res.send(saver)
     } catch (error) {
-        res.send(false)
+        res.send("error")
     }
      
        
 })
 
-appGettUser.get("/requests",(req,res,next)=>{
-    res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
-    res.header({"Access-Control-Allow-Credentials": true});
-    res.header("Access-Control-Max-Age", 24*60*60*1000);
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+appGettUser.get("/requests"
+// ,(req,res,next)=>{
+//     res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
+//     res.header({"Access-Control-Allow-Credentials": true});
+//     res.header("Access-Control-Max-Age", 24*60*60*1000);
+//       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
 
-    const sender = req.cookies.token
-  // console.log(sender)
-  if(!sender) return res.send("not authenticated");
-  const decoder =  jwt.verify(sender,process.env.MYSECRET)
+//     const sender = req.cookies.token
+//   // console.log(sender)
+//   if(!sender) return res.send("not authenticated");
+//   const decoder =  jwt.verify(sender,process.env.MYSECRET)
   
-if(!decoder) return res.send("not authenticated");
-next()}
+// if(!decoder) return res.send("not authenticated");
+// next()}
 ,async (req,res)=>{
     
         

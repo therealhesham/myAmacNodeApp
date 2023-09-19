de8ed86d6cb267e1d755a407f9611e8aa814d465
therealhesham <heshammoha231992@gmail.com>
2023-09-20 00:52:43 +0300

    commit on master

--- modules/login.js
+++ modules/login.js
@@ -34,30 +34,7 @@ next()
 })
 
 
-appLogin.post("/login",(req,res,next)=>{
-    res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
-    res.header({"Access-Control-Allow-Credentials": true});
-    res.header("Access-Control-Max-Age", 24*60*60*1000);
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-    res.header("Set-Cookie", "sid=14A52; max-age=3600;samesite=None;sameSite=none ;SameSite=None ;Secure ")
-    res.cookie("token","jwter"
-,{
-      maxAge: 1000000000 , sameSite : "None",SameSite:"None" ,
-      samesite : "None",SameSite:"none" ,
-      sameSite : "None",SameSite:"None" 
-}
-);
-
-
-next()    
-
-
-}, async (req,res)=>{
-// req.session.name ="hesham"
-// res.set({"Access-Control-Allow-Origin": "https://my-amac-react-app.vercel.app"});
-// res.set({"Access-Control-Allow-Credential": true});
-res.header({"Access-Control-Allow-Origin": "https://my-amac-react-app.vercel.app"});
+appLogin.post("/login", async (req,res)=>{
 
 
 const email = req.body.email

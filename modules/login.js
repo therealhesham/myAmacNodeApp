5bc25703b29249efeb284edd39aaf9a69b7439ec
therealhesham <heshammoha231992@gmail.com>
2023-09-20 00:48:43 +0300

    commit on master

--- modules/login.js
+++ modules/login.js
@@ -10,6 +10,28 @@ appLogin=express()
 appLogin.use(cors({maxAge:24*60*60*1000,origin:"https://my-amac-react-app.vercel.app" ,exposedHeaders:'*',credentials:true,preflightContinue: true}));
 appLogin.use(express.json())
 // appLogin.use(session({resave:false,secret:'session',cookie:{maxAge:1000*60*60,sameSite:"none",secure:true}}))
+appLogin.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
+  res.header({"Access-Control-Allow-Credentials": true});
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override,Content-Type, Accept');
+  res.header("Access-Control-Max-Age", 24*60*60*1000);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  res.header("Set-Cookie", "sid=14A52; max-age=3600;samesite=None;sameSite=none ;SameSite=None ;Secure ")
+  res.cookie("token","jwter"
+,{
+    maxAge: 1000000000 , sameSite : "None",SameSite:"None" ,
+    samesite : "None",SameSite:"none" ,
+    sameSite : "None",SameSite:"None" 
+}
+);
+
+
+next()    
+
+
+
+})
 
 
 appLogin.post("/login",(req,res,next)=>{

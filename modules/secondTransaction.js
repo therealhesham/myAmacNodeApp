ea4001cbb4e4292ed8be942c775fdf2a42e9b6a3
therealhesham <heshammoha231992@gmail.com>
2023-10-22 14:48:35 +0300

    commit on master

--- modules/secondTransaction.js
+++ modules/secondTransaction.js
@@ -291,23 +291,7 @@ const datar = new recyclebin({
     
     )
     
-    appSecondTransaction.get("/find/:name/store/:store",(req,res,next)=>{
-      res.header("Access-Control-Allow-Origin", "https://my-amac-react-app.vercel.app");
-      res.header({"Access-Control-Allow-Credentials": true});
-      res.header("Access-Control-Max-Age", 24*60*60*1000);
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  
-      const sender = req.cookies.token
-    // console.log(sender)
-    if(!sender) return res.send("not authenticated");
-    const decoder =  jwt.verify(sender,process.env.MYSECRET)
-    
-  if(!decoder) return res.send("not authenticated");
-  if(!decoder.isAdmin) return res.send("not authenticated");
-  next()}
-  
-  ,async(req,res)=>{
+    appSecondTransaction.get("/find/:name/store/:store",async(req,res)=>{
   
   /*
   transaction of stores usualy from(preview Schema) to (destination will 
